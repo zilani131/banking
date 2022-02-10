2f5fb390f089454c9ef8f1b33ac5ac4847db89a2
zilani131 <zilani.cuet@gmail.com>
2022-02-10 18:36:05 +0600

    five

--- banking.js
+++ banking.js
@@ -12,3 +12,17 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
      const totalBalance=currentDepositValue+previousBalaceValue;
      document.getElementById('previous-balance-text').innerText=totalBalance;
 })
+// withdraw button
+document.getElementById('withdraw-btn').addEventListener('click',function(){
+    const previousWithdrawText=document.getElementById('previous-withdraw-text').innerText;
+    const previousWithdrawValue=parseFloat( previousWithdrawText)
+    const currentWithdrawText=document.getElementById('current-withdraw-text').value;
+    const currentWithdrawValue=parseFloat(currentWithdrawText)
+    const totalWithdrawValue=previousWithdrawValue+currentWithdrawValue;
+    document.getElementById('previous-withdraw-text').innerText=totalWithdrawValue;
+    document.getElementById('current-withdraw-text').value=''  
+     const previousBalanceText=document.getElementById('previous-balance-text').innerText;
+     const previousBalaceValue=parseFloat(previousBalanceText)
+     const totalBalance=previousBalaceValue-currentWithdrawValue;
+     document.getElementById('previous-balance-text').innerText=totalBalance;
+})
